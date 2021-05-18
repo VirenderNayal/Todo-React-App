b1ee78694ee14a00ccd0689be61b321bddf54ce4
Virender Nayal <virendernayal3003@gmail.com>
2021-05-18 20:32:53 +0530

    Added key prop, Basic styling -v1.0.1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Button, FormControl, FormHelperText, TextField } from '@material-ui/core';
+import { Button, Container, FormControl, FormHelperText, List, Paper, TextField } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
 import firebase from 'firebase';
 import { db } from './firebase';
@@ -14,7 +14,7 @@ function App() {
   useEffect(() => {
     // Fires when app loads
     db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
-      setTodos(snapshot.docs.map(doc => ({id: doc.id, todo: doc.data().todo, timestamp: doc.data().timestamp})));
+      setTodos(snapshot.docs.map(doc => ({ id: doc.id, todo: doc.data().todo, timestamp: doc.data().timestamp })));
     })
   }, [])
 
@@ -31,34 +31,35 @@ function App() {
 
   return (
     <div className="App">
-      <h1> React-Todo-App </h1>
-      <br />
-      <form>
-        <FormControl style = {{ width : '100%' }}>
+      <h1 style={{ fontFamily : 'monospace', fontSize : '2.5rem' }} > React-Todo-App </h1>
+      <form style = {{ margin : '2rem' }} >
+        <FormControl style={{ width: '100%' }}>
           <TextField
             id=""
             label="Write a Todo"
             value={input}
             onChange={event => setInput(event.target.value)}
             variant='outlined'
-            style ={{ width : '50%', margin: 'auto' }}
+            style={{ width: '50%', margin: 'auto' }}
           />
-          <FormHelperText style ={{ width : '50%', margin: 'auto' }}>Anything you want to rembember</FormHelperText>
+          <FormHelperText style={{ width: '50%', margin: 'auto' }}>Anything you want to rembember</FormHelperText>
         </FormControl>
-        <br/>
-        <br/>
-        <Button type='submit' onClick={ addTodo } variant="contained" color="primary" disabled={!input} endIcon ={<SendIcon />} >
+        <Button type='submit' onClick={addTodo} variant="contained" color="primary" disabled={!input} endIcon={<SendIcon />} >
           Add Todo
         </Button>
       </form>
 
-      <ul>
-        {
-          todos.map(todo => (
-            <Todo todo={todo} />
-          ))
-        }
-      </ul>
+      <Container>
+        <Paper elevation={3}>
+          <List>
+            {
+              todos.map(todo => (
+                <Todo todo={todo} key={todo.id} />
+              ))
+            }
+          </List>
+        </Paper>
+      </Container>
     </div>
   );
 }
